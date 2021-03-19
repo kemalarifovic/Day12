b531c8a2f20d55f1512d32c8f24f83d57f26ac87
Kemal Arifovic <gigalab@gmail.com>
2021-03-19 11:01:04 +0100

    Fibonacci Array

---
+++ ex04/src/index.js
@@ -0,0 +1,26 @@
+// Only change code below this line
+function sumFibonacci(num){
+ 
+        let prevNumber = 0;
+        let currNumber = 1;
+        let result = 0;
+        while (currNumber <= num) {
+          if (currNumber % 2 !== 0) {
+            result += currNumber;
+          }
+          currNumber += prevNumber;
+          prevNumber = currNumber - prevNumber;
+        }
+      
+        return result;
+  
+}
+// Only change code above this line
+
+console.log(sumFibonacci(1));
+console.log(sumFibonacci(10));
+console.log(sumFibonacci(20));
+console.log(sumFibonacci(4));
+console.log(sumFibonacci(-5));
+
+module.exports = sumFibonacci;
